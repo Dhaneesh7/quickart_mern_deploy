6f9982c12a97958e9b21c9c857f0221a3d134b19
Dhaneesh7 <dhaneeshvj5@gmail.com>
2025-07-25 14:38:25 +0530

    update backend/src

--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const orders=require('../controllers/orderController');
 const { protectRoute } = require('../middleware/Auth_Middleware');
-router.get('/confirm', protectRoute, orders.confirmOrder); // Confirm order endpoint
+router.get('/confirm', orders.confirmOrder); // Confirm order endpoint
 router.get('/:userId',orders.getOrder);
 router.post('/:userId',orders.addOrder);
 router.delete('/:userId/:orderId', protectRoute,orders.removeFromOrder);
